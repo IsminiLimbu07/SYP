2d3ba6d7efef7800850580c80b901c84f2abb809
IsminiLimbu07 <isminilaksamba38@gmail.com>
2025-11-23 11:47:07 +0545

    Cors added

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { sql } from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
