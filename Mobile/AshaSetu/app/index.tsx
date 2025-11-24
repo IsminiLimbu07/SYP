e6a6d2c40161ae0861af361fc6aef54b2924f7c9
IsminiLimbu07 <isminilaksamba38@gmail.com>
2025-11-24 18:32:40 +0545

    Frontend Started

---
+++ Mobile/AshaSetu/app/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "red"
+      }}
+    >
+      <Text style= {{ color: "yellow" } }>Edit app/index.tsx to edit this screen.</Text>
+      <Link href={"/about"}>About</Link>
+
+
+    </View>
+  );
+}
