0154334a50d86381b32ff6f9e9ec09fcc86336be
Nicreater <zoroisdope@gmail.com>
2026-03-20 17:21:02 +0545

    added admin middleware

---
+++ Backend/middleware/AdminMiddleware.js
@@ -0,0 +1,9 @@
+export const isAdmin = (req, res, next) => {
+    if (!req.user?.is_admin) {
+        return res.status(403).json({
+            success: false,
+            message: "Access denied. Admins only."
+        });
+    }
+    next();
+};
